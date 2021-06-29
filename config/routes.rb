85b30bc27f1600f131064c22ba2b162b107c2fc0
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-29 21:05:25 +0800

    added buyer_stocks resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :buyers, :brokers, :admins
-  resources :buyers, :brokers, :admins, :users, :broker_stocks, :stocks
+  resources :buyers, :brokers, :admins, :users, :broker_stocks, :buyer_stocks, :stocks
 
   root 'home#index'
 
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
    get '/admins/new_buyer' => 'admins#new_buyer'
    post '/admins/new_broker' => 'admins#create_new_broker'
    post '/admins/new_buyer' => 'admins#create_new_buyer'
-  
 end
