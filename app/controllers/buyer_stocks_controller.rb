10d23e03d1ed9c08905b1ad7e455806442dd2de7
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-02 01:29:48 +0800

    enabled adding quantities to existing stocks in buyer's portfolio

--- app/controllers/buyer_stocks_controller.rb
+++ app/controllers/buyer_stocks_controller.rb
@@ -2,13 +2,14 @@ class BuyerStocksController < ApplicationController
   def index; end
 
   def create
-    @buyer_stock = BuyerStock.new(buyer_stock_params)
-    @balance = params[:buyer_stock][:quantity].to_f * params[:buyer_stock][:price].to_f
-
-    if @buyer_stock.valid?
+    @cost = params[:buyer_stock][:quantity].to_f * params[:buyer_stock][:price].to_f
+    # check if stock already exists in portfolio
+    
+    if not_added?(current_buyer.id, params[:buyer_stock][:stock_id])
+      @buyer_stock = BuyerStock.new(buyer_stock_params)
       # update balance
-      if current_buyer.balance >= @balance
-        current_buyer.balance -= @balance
+      if current_buyer.balance >= @cost
+        current_buyer.balance -= @cost
         current_buyer.save
         @buyer_stock.save
         redirect_to root_path, notice: 'Stock was added to the Portfolio.'
@@ -18,11 +19,25 @@ class BuyerStocksController < ApplicationController
 
       else
         redirect_to root_path, alert: 'Insufficient balance'
-      end 
+      end
     
     else
-      redirect_to root_path, alert: @buyer_stock.errors.messages.to_s
+      # update balance and buyer_stock
+      if current_buyer.balance >= @cost
+        current_buyer.balance -= @cost
+        current_buyer.save
+
+        #update buyer_stock
+        @existing_stock = BuyerStock.find_by(user_id: current_buyer.id, stock_id: params[:buyer_stock][:stock_id])
+        @existing_stock.quantity += params[:buyer_stock][:quantity].to_i
+        @existing_stock.save
+        redirect_to root_path, notice: 'Stock was added to the Portfolio.'
+      end
+    
+    
+      
     end
+    # redirect_to root_path, alert: @buyer_stock.errors.messages.to_s
   end
 
   def update
@@ -31,6 +46,8 @@ class BuyerStocksController < ApplicationController
   end
 
   def destroy
+    # currently, destroying buyer stock means selling the stock so buyer account receives an amount of quantity*price
+
     # create transact instance
     @stock = BuyerStock.find_by(user_id: current_user.id, stock_id: params[:id])
     @transact = current_buyer.transacts.build(broker_id: current_user.id, buyer_id: nil, stock_id: params[:id], quantity: @stock.quantity, price: @stock.price)
@@ -45,7 +62,15 @@ class BuyerStocksController < ApplicationController
     current_buyer.save
   end
 
+
+
+  private
   def buyer_stock_params
     params.require(:buyer_stock).permit(:user_id, :stock_id, :companyname, :quantity, :price, :broker_id)
+  end
+
+  def not_added?(user_id, stock_id)
+    res = BuyerStock.find_by(user_id: user_id, stock_id: stock_id)
+    res.nil?
   end
 end
