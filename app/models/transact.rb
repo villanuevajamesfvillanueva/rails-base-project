0421bfe6241cf1ac959055c34ef21d9cdeb15b8b
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-03 01:45:13 +0800

    updated user_id to broker_id and buyer_id

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -1,6 +1,6 @@
 class Transact < ApplicationRecord
-  belongs_to :buyer, class_name: 'User', inverse_of: false
-  belongs_to :broker, class_name: 'User', inverse_of: false
+  belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id, inverse_of: false
+  belongs_to :broker, class_name: 'User', foreign_key: :broker_id, inverse_of: false
   has_many :stock, class_name: 'Stock', foreign_key: :stock_id, dependent: :nullify, inverse_of: false
 
   validates :stock_id, presence: true
