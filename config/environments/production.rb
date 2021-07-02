c48db4d414ca547b3b05d9b8b1d0b837791b701b
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-02 23:41:10 +0800

    sets action_mailer.raise_delivery_errors = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   host = 'stock-app-stonks.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 #   ActionMailer::Base.smtp_settings = {
