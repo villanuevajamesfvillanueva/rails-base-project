89a0c49d408c13f7b28bbe200e2467f4392475cd
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-02 20:49:20 +0800

    removed duplicate migration file before users table creation

--- db/migrate/20210619020529_updating_user_table.rb
+++
@@ -1,9 +0,0 @@
-class UpdatingUserTable < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :approved, :boolean
-    add_column :users, :confirmable, :boolean
-    add_column :users, :type, :string
-  end
-end
- 
